e865abf6c5d705f22c81e6dc746a71bbae6ca4a0
Austin Smart <asmart018@gmail.com>
2022-11-16 21:36:35 -0500

    JS_Variables Complete

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
-
-// Exercise 3
+const firstName = "Austin";
+let lastName = "Smart";
+var age = 27;
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pagecount = 722;
+let bookmark = 456;
+let hasRead = true;
